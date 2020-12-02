bfe0b59344c0408a715ca3e38c33c0314577671f
dialtone <dialtone@adroll.com>
2020-12-02 00:18:43 -0800

    day 2 solved, and quite quick as well, added a bunch of useful deps

--- benches/day02.rs
+++ benches/day02.rs
@@ -2,11 +2,11 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use aoc::solutions::year2020::day02::{parse, part1, part2};
 
-fn bench_day1(c: &mut Criterion) {
-    let expenses = parse(aoc::input::get_input(2020, 2).unwrap());
-    c.bench_function("day2 part 1", |b| b.iter(|| part1(&expenses)));
-    c.bench_function("day2 part 2", |b| b.iter(|| part2(&expenses)));
+fn bench_day2(c: &mut Criterion) {
+    let raw_input = aoc::input::get_input(2020, 2).unwrap();
+    c.bench_function("day2 part 1", |b| b.iter(|| part1(&raw_input)));
+    c.bench_function("day2 part 2", |b| b.iter(|| part2(&raw_input)));
 }
 
-criterion_group!(benches, bench_day1);
+criterion_group!(benches, bench_day2);
 criterion_main!(benches);
--- src/solutions/year2020/day02.rs
+++ src/solutions/year2020/day02.rs
@@ -1,14 +1,46 @@
 use super::*;
+use std::iter::Iterator;
 
-pub fn part1(input: &Vec<u32>) {}
+pub fn part1(input: &String) -> usize {
+    // time:   [6.6843 us 6.8296 us 6.9876 us] without parsing
+    parse(input)
+        .filter(|row| (row.0..=row.1).contains(&bytecount::count(row.3, row.2 as u8)))
+        .count()
 
-pub fn part2(input: &Vec<u32>) {}
+    // time:   [71.867 us 73.218 us 74.751 us] without parsing
+    // let mut valid = 0;
+    // for row in input {
+    //     let v = (row.3).matches(&row.2).count();
+    //     if v >= row.0 && v <= row.1 {
+    //         valid += 1;
+    //     }
+    // }
+    // return valid;
+}
 
-pub fn parse(s: String) -> Vec<u32> {
+pub fn part2(input: &String) -> usize {
+    // time:   [1.9534 us 1.9681 us 1.9845 us] without parsing
+    parse(input)
+        .filter(|row| (row.3[row.0 - 1] == row.2 as u8) != (row.3[row.1 - 1] == row.2 as u8))
+        .count()
+
+    // time:   [31.714 us 32.140 us 32.650 us] without parsing
+    // let mut valid = 0;
+    // for row in input {
+    //     let sentinel = row.2.chars().nth(0).unwrap();
+    //     let test1 = row.3.chars().nth(row.0 - 1).unwrap() == sentinel;
+    //     let test2 = row.3.chars().nth(row.1 - 1).unwrap() == sentinel;
+    //     if (test1 && !test2) || (!test1 && test2) {
+    //         valid += 1;
+    //     }
+    // }
+    // return valid;
+}
+
+pub fn parse(s: &String) -> impl Iterator<Item = (usize, usize, char, &[u8])> {
     s.trim()
-        .split("\n")
-        .map(|x| x.parse::<u32>().unwrap())
-        .collect()
+        .lines()
+        .map(|x| scan!("{}-{} {}: {}" <- x).unwrap())
 }
 
 #[cfg(test)]
@@ -17,16 +49,18 @@ mod tests {
 
     #[test]
     fn day02_test() {
-        let expense_report = vec![1721, 979, 366, 299, 675, 1456];
-        assert_eq!(part1(&expense_report), 514579);
-        assert_eq!(part2(&expense_report), 241861950);
+        let test_input = "1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc"
+            .to_owned();
+        assert_eq!(part1(&test_input), 2);
+        assert_eq!(part2(&test_input), 1);
     }
 
     #[test]
     fn day02() {
-        let input = get_input(2020, 1).unwrap();
-        let expense_report = parse(input);
-        assert_eq!(part1(&expense_report), 876459);
-        assert_eq!(part2(&expense_report), 116168640);
+        let input = get_input(2020, 2).unwrap();
+        assert_eq!(part1(&input), 422);
+        assert_eq!(part2(&input), 451);
     }
 }

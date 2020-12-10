a242eb5079dcfb7f69212e2fae121fca4ea60aef
dialtone <dialtone@adroll.com>
2020-12-09 23:42:08 -0800

    optimize

--- src/solutions/year2020/day10.rs
+++ src/solutions/year2020/day10.rs
@@ -1,19 +1,19 @@
-// day10 parse             time:   [2.1756 us 2.1950 us 2.2159 us]
-// day10 part 1            time:   [1.2428 us 1.2577 us 1.2739 us]
-// day10 part 2            time:   [1.9422 us 1.9579 us 1.9796 us]
-
+// day10 parse             time:   [2.1125 us 2.1386 us 2.1670 us]
+// day10 part 1            time:   [848.92 ns 859.54 ns 872.40 ns]
+// day10 part 2            time:   [1.1545 us 1.1669 us 1.1816 us]
 use super::*;
 
 use num_integer::binomial;
 
 type Input = String;
-type Parsed = Vec<usize>;
+type Parsed = Vec<u64>;
 
 pub fn part1(oinput: &Parsed) -> usize {
     let mut input = oinput.clone();
     input.push(0);
-    input.push(input.iter().max().unwrap() + 3);
-    input.sort();
+    input.sort_unstable();
+    input.push(input[input.len() - 1] + 3);
+
     let mut diff3 = 0;
     let mut diff1 = 0;
     for i in 1..input.len() {
@@ -32,37 +32,35 @@ pub fn part1(oinput: &Parsed) -> usize {
 pub fn part2(oinput: &Parsed) -> usize {
     let mut input = oinput.clone();
     input.push(0);
-    input.push(input.iter().max().unwrap() + 3);
-    input.sort();
+    input.sort_unstable();
+    input.push(input[input.len() - 1] + 3);
+
     let mut diff1_sequence = 0;
-    let mut replaceable = 1;
+    let mut combos = 1;
     for i in 1..input.len() {
         let diff = input[i] - input[i - 1];
         if diff == 3 {
-            if diff1_sequence > 2 {
-                let removable = diff1_sequence - 2;
-                let mut prod = binomial(removable, 0) + binomial(removable, 1);
+            if diff1_sequence > 1 {
+                let removable = diff1_sequence - 1;
+                // 1 is binomial(removable, 0) + removable is binomial(removable, 1)
+                let mut prod = 1 + removable;
                 if removable >= 2 {
                     prod += binomial(removable, 2);
                 }
-                replaceable *= prod;
+                combos *= prod;
             }
             diff1_sequence = 0;
         } else if diff == 1 {
-            if diff1_sequence == 0 {
-                // first one is always part of the sequence
-                diff1_sequence += 1;
-            }
             diff1_sequence += 1;
         } else {
             unreachable!()
         }
     }
-    replaceable
+    combos
 }
 
 pub fn parse(s: &Input) -> Parsed {
-    s.lines().map(|x| x.parse::<usize>().unwrap()).collect()
+    s.lines().map(|x| x.parse().unwrap()).collect::<Vec<_>>()
 }
 
 #[cfg(test)]

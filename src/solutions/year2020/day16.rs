49d0ce3f20318faca4139d85bb7b7e5cca2eb3ed
dialtone <dialtone@adroll.com>
2020-12-16 00:55:15 -0800

    improve a couple of things performance wise and clean up

--- src/solutions/year2020/day16.rs
+++ src/solutions/year2020/day16.rs
@@ -1,50 +1,27 @@
-// day16 parse             time:   [227.19 us 231.01 us 235.46 us]
-// day16 part 1            time:   [51.266 us 51.837 us 52.489 us]
-// day16 part 2            time:   [432.77 us 436.99 us 441.51 us]
+// day16 parse             time:   [235.90 us 241.36 us 247.52 us]
+// day16 part 1            time:   [52.535 us 53.690 us 55.173 us]
+// day16 part 2            time:   [266.19 us 270.68 us 275.73 us]
 
 use super::*;
+use std::collections::{BTreeMap, BTreeSet};
 use std::ops::RangeInclusive;
-use std::{
-    collections::{BTreeMap, BTreeSet},
-    fmt::write,
-};
 
 type Input = String;
-type Parsed = String;
 
 pub fn part1(
-    my_ticket: &Vec<usize>,
+    _my_ticket: &Vec<usize>,
     tickets: &Vec<Vec<usize>>,
     rules: &BTreeMap<&str, (RangeInclusive<usize>, RangeInclusive<usize>)>,
 ) -> usize {
-    // tickets
-    //     .iter()
-    //     .flatten()
-    //     .filter(|field| {
-    //         rules
-    //             .values()
-    //             .all(|(range1, range2)| !range1.contains(&field) && !range2.contains(&field))
-    //     })
-    //     .sum()
-    let mut total = 0;
-    // let mut invalid = 0;
-    for ticket in tickets {
-        for field in ticket {
-            let mut pass = false;
-            for (range1, range2) in rules.values() {
-                if range1.contains(field) || range2.contains(field) {
-                    pass = true;
-                    break;
-                }
-            }
-            if !pass {
-                total += field;
-                // invalid += 1;
-            }
-        }
-    }
-    // println!("total invalid {}", invalid);
-    total
+    tickets
+        .iter()
+        .flatten()
+        .filter(|field| {
+            rules
+                .values()
+                .all(|r| !r.0.contains(&field) && !r.1.contains(&field))
+        })
+        .sum()
 }
 
 pub fn part2(
@@ -53,7 +30,8 @@ pub fn part2(
     rules: &BTreeMap<&str, (RangeInclusive<usize>, RangeInclusive<usize>)>,
 ) -> usize {
     let mut product = 1;
-    let mut valid_tickets: Vec<Vec<usize>> = Vec::new();
+    // Made this Vec<&[size]> instead of Vec<Vec<usize>> to avoid borrow checker anger
+    let mut valid_tickets: Vec<&[usize]> = Vec::new();
     for ticket in tickets {
         let mut pass = false;
         for field in ticket {
@@ -69,7 +47,7 @@ pub fn part2(
             }
         }
         if pass {
-            valid_tickets.push(ticket.clone());
+            valid_tickets.push(ticket);
         }
     }
 
@@ -78,11 +56,8 @@ pub fn part2(
     // 1 applicable column per field
     let mut candidates = BTreeMap::new();
     for (rule_type, (range1, range2)) in rules.iter() {
-        // println!("Considering field {}", rule_type);
         // apply the rules vertically in the tickets for each ticket
         for col in 0..col_num {
-            // println!("Considering candidate column {}", col);
-
             // given column id above, apply the rule to each ticket in the list
             // and if all values pass then this is a candidate
             let mut pass = true;
@@ -90,20 +65,13 @@ pub fn part2(
                 let candidate = ticket[col];
                 pass = range1.contains(&candidate) || range2.contains(&candidate);
                 if !pass {
-                    // println!(
-                    //     "{} didn't satisfy this field rule {:?} or {:?}",
-                    //     candidate, range1, range2
-                    // );
                     break;
                 }
             }
+
             // so if this rule passed for all values in the column
             // and wasn't already assigned to something, put it in candidates
             if pass {
-                // println!(
-                //     "column {} satisfied so adding to possible candidates for field {}",
-                //     col, rule_type
-                // );
                 candidates
                     .entry(rule_type)
                     .or_insert(BTreeSet::new())
@@ -111,35 +79,21 @@ pub fn part2(
             }
         }
     }
-    // println!("---------");
-    // for (&column, field) in candidates.iter() {
-    //     println!("Column {} has candidates {:?}", column, field);
-    // }
 
-    // println!("---------");
     // do multiple passes to remove anything that already appears in other sets
-    while !candidates.iter().all(|(_, v)| v.len() == 1) {
+    while !candidates.iter().any(|(_, v)| v.len() > 1) {
+        // this really punishes performance
         let sets = candidates.clone();
-        for (&k1, v) in sets {
-            if v.len() == 1 {
-                // println!(
-                //     "Column {} is assigned field number {}",
-                //     k1,
-                //     v.iter().next().unwrap()
-                // );
-                candidates.iter_mut().for_each(|(&&k2, v1)| {
-                    if k2 != k1 && v1.len() > 1 {
-                        v1.remove(v.iter().next().unwrap());
+        for (&k1, v1) in sets {
+            if v1.len() == 1 {
+                candidates.iter_mut().for_each(|(&&k2, v2)| {
+                    if k2 != k1 && v2.len() > 1 {
+                        v2.remove(v1.iter().next().unwrap());
                     }
                 });
             }
         }
     }
-
-    // println!("--------- FINAL ASSIGNMENTS ------");
-    // for (&column, field) in candidates.iter() {
-    //     println!("Column {} is assigned {:?}", column, field);
-    // }
 
     for (&column, field) in candidates.iter() {
         if column.starts_with("departure") {
@@ -197,38 +151,38 @@ pub fn parse(
 mod tests {
     use super::*;
 
-    //     #[test]
-    //     fn test_day16() {
-    //         let test_input = "class: 1-3 or 5-7
-    // row: 6-11 or 33-44
-    // seat: 13-40 or 45-50
+    #[test]
+    fn test_day16() {
+        let test_input = "class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
 
-    // your ticket:
-    // 7,1,14
+your ticket:
+7,1,14
 
-    // nearby tickets:
-    // 7,3,47
-    // 40,4,50
-    // 55,2,20
-    // 38,6,12"
-    //             .to_owned();
-    //         let (myt, ts, rules) = parse(&test_input);
-    //         assert_eq!(part1(&myt, &ts, &rules), 71);
-    //         let test_input = "departure class: 0-1 or 4-19
-    // departure row: 0-5 or 8-19
-    // seat: 0-13 or 16-19
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12"
+            .to_owned();
+        let (myt, ts, rules) = parse(&test_input);
+        assert_eq!(part1(&myt, &ts, &rules), 71);
+        let test_input = "departure class: 0-1 or 4-19
+departure row: 0-5 or 8-19
+seat: 0-13 or 16-19
 
-    // your ticket:
-    // 11,12,13
+your ticket:
+11,12,13
 
-    // nearby tickets:
-    // 3,9,18
-    // 15,1,5
-    // 5,14,9"
-    //             .to_owned();
-    //         let (myt, ts, rules) = parse(&test_input);
-    //         assert_eq!(part2(&myt, &ts, &rules), 132);
-    //     }
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9"
+            .to_owned();
+        let (myt, ts, rules) = parse(&test_input);
+        assert_eq!(part2(&myt, &ts, &rules), 132);
+    }
 
     #[test]
     fn day16() {
@@ -236,6 +190,6 @@ mod tests {
         let (myt, ts, rules) = parse(&input);
         assert_eq!(part1(&myt, &ts, &rules), 28873);
         let (myt, ts, rules) = parse(&input);
-        assert_eq!(part2(&myt, &ts, &rules), 5);
+        assert_eq!(part2(&myt, &ts, &rules), 56587952167);
     }
 }

acf7ea3252c4547f5946005d07ce0d35c9856253
dialtone <dialtone@adroll.com>
2023-02-22 20:40:42 -0800

    input for day 13

--- src/solutions/year2022/day11.rs
+++ src/solutions/year2022/day11.rs
@@ -1,19 +1,18 @@
 // use crate::solutions::clear_screen;
-// use crate::solutions::g#![feature(portable_simd)]o_to_top;
-use std::simd::u64x2;
+// use crate::solutions::go_to_top;
 
 pub struct Monkey {
     pub stack: Vec<u64>,
     pub op: Op,
-    pub div: u64x2,
+    pub div: u64,
     pub conditions: (usize, usize),
 }
 
 #[derive(Clone, Copy)]
 pub enum Op {
     Square,
-    Add(u64x2),
-    Mul(u64x2),
+    Add(u64),
+    Mul(u64),
 }
 
 pub fn parse(input: &[u8]) -> Vec<Monkey> {
@@ -43,8 +42,8 @@ pub fn parse(input: &[u8]) -> Vec<Monkey> {
         let num = op_and_num.next().unwrap();
         let operation: Op = match (op[0], num) {
             (b'*', b"old") => Op::Square,
-            (b'*', n) => Op::Mul(u64x2::splat(atoi::atoi(n).unwrap())),
-            (b'+', n) => Op::Add(u64x2::splat(atoi::atoi(n).unwrap())),
+            (b'*', n) => Op::Mul(atoi::atoi(n).unwrap()),
+            (b'+', n) => Op::Add(atoi::atoi(n).unwrap()),
             _ => {
                 unreachable!();
             }
@@ -52,7 +51,7 @@ pub fn parse(input: &[u8]) -> Vec<Monkey> {
 
         // divisible test
         line = input.next().unwrap();
-        let divisible = u64x2::splat(atoi::atoi(&line[21..]).unwrap());
+        let divisible = atoi::atoi(&line[21..]).unwrap();
 
         // true condition
         line = input.next().unwrap();
@@ -85,10 +84,10 @@ pub fn part1(input: &[u8]) -> usize {
                 inspections[i] += 1;
                 let new_item = match monkeys[i].op {
                     Op::Square => item * item / 3,
-                    Op::Mul(n) => item * n[0] / 3,
-                    Op::Add(n) => (item + n[0]) / 3,
+                    Op::Mul(n) => item * n / 3,
+                    Op::Add(n) => (item + n) / 3,
                 };
-                let throw_to = if new_item % monkeys[i].div[0] == 0 {
+                let throw_to = if new_item % monkeys[i].div == 0 {
                     monkeys[i].conditions.0
                 } else {
                     monkeys[i].conditions.1
@@ -111,109 +110,65 @@ pub fn part2(input: &[u8]) -> usize {
     // pointer to last item in items for each monkey
     let mut m_cnt = [0usize; 8];
 
-    let mcm = monkeys.iter().map(|m| m.div[0]).product::<u64>();
+    let mcm = monkeys.iter().map(|m| m.div).product::<u64>();
     for (n, m) in monkeys.iter().enumerate() {
         for i in m.stack.iter() {
             items[n][m_cnt[n]] = *i;
             m_cnt[n] += 1;
         }
     }
-    let mcm = u64x2::splat(mcm);
 
     for _ in 0..10_000 {
         for i in 0..monkeys.len() {
+            let mut itz = items[i];
+            let l = m_cnt[i];
+            inspections[i] += l;
+            let test = monkeys[i].div;
             match monkeys[i].op {
                 Op::Square => {
-                    let n = u64x2::splat(1);
-                    for v in (0..m_cnt[i]).step_by(n.lanes()) {
-                        let val = u64x2::from_slice(&items[i][v..v + 2]);
-                        let new_item = val * val % mcm;
-
-                        let new_rem = new_item % monkeys[i].div;
-                        let throw_to = if new_rem[0] == 0 {
+                    for it in itz.iter_mut().take(l) {
+                        *it *= *it;
+                        *it %= mcm;
+                        let throw_to = if *it % test == 0 {
                             monkeys[i].conditions.0
                         } else {
                             monkeys[i].conditions.1
                         };
-
-                        let throw_to1 = if new_rem[1] == 0 {
-                            monkeys[i].conditions.0
-                        } else {
-                            monkeys[i].conditions.1
-                        };
-
-                        inspections[i] += 1;
-                        items[throw_to][m_cnt[throw_to]] = new_item[0];
-                        m_cnt[throw_to] += 1;
-                        if v + 1 < m_cnt[i] {
-                            inspections[i] += 1;
-                            items[throw_to1][m_cnt[throw_to1]] = new_item[1];
-                            m_cnt[throw_to1] += 1;
-                        }
+                        let c = &mut m_cnt[throw_to];
+                        items[throw_to][*c] = *it;
+                        *c += 1;
                     }
-                    m_cnt[i] = 0;
                 }
                 Op::Mul(n) => {
-                    for v in (0..m_cnt[i]).step_by(n.lanes()) {
-                        let val = u64x2::from_slice(&items[i][v..v + 2]);
-                        let new_item = val * n % mcm;
-
-                        let new_rem = new_item % monkeys[i].div;
-                        let throw_to = if new_rem[0] == 0 {
+                    for it in itz.iter_mut().take(l) {
+                        *it *= n;
+                        *it %= mcm;
+                        let throw_to = if *it % test == 0 {
                             monkeys[i].conditions.0
                         } else {
                             monkeys[i].conditions.1
                         };
-
-                        let throw_to1 = if new_rem[1] == 0 {
-                            monkeys[i].conditions.0
-                        } else {
-                            monkeys[i].conditions.1
-                        };
-
-                        inspections[i] += 1;
-                        items[throw_to][m_cnt[throw_to]] = new_item[0];
-                        m_cnt[throw_to] += 1;
-
-                        if v + 1 < m_cnt[i] {
-                            inspections[i] += 1;
-                            items[throw_to1][m_cnt[throw_to1]] = new_item[1];
-                            m_cnt[throw_to1] += 1;
-                        }
+                        let c = &mut m_cnt[throw_to];
+                        items[throw_to][*c] = *it;
+                        *c += 1;
                     }
-                    m_cnt[i] = 0;
                 }
                 Op::Add(n) => {
-                    for v in (0..m_cnt[i]).step_by(n.lanes()) {
-                        let val = u64x2::from_slice(&items[i][v..v + 2]);
-                        let new_item = val + n % mcm;
-
-                        let new_rem = new_item % monkeys[i].div;
-                        let throw_to = if new_rem[0] == 0 {
+                    for it in itz.iter_mut().take(l) {
+                        *it += n;
+                        *it %= mcm;
+                        let throw_to = if *it % test == 0 {
                             monkeys[i].conditions.0
                         } else {
                             monkeys[i].conditions.1
                         };
-
-                        let throw_to1 = if new_rem[1] == 0 {
-                            monkeys[i].conditions.0
-                        } else {
-                            monkeys[i].conditions.1
-                        };
-
-                        inspections[i] += 1;
-                        items[throw_to][m_cnt[throw_to]] = new_item[0];
-                        m_cnt[throw_to] += 1;
-
-                        if v + 1 < m_cnt[i] {
-                            inspections[i] += 1;
-                            items[throw_to1][m_cnt[throw_to1]] = new_item[1];
-                            m_cnt[throw_to1] += 1;
-                        }
+                        let c = &mut m_cnt[throw_to];
+                        items[throw_to][*c] = *it;
+                        *c += 1;
                     }
-                    m_cnt[i] = 0;
                 }
-            };
+            }
+            m_cnt[i] = 0;
         }
     }
     inspections.sort();
@@ -226,7 +181,7 @@ mod tests {
     use crate::solutions::*;
 
     #[test]
-    fn test_example() {
+    fn day11_test() {
         let input = "Monkey 0:
   Starting items: 79, 98
   Operation: new = old * 19
@@ -259,7 +214,7 @@ Monkey 3:
     }
 
     #[test]
-    fn test() {
+    fn day11() {
         let input = get_input(2022, 11).unwrap();
         assert_eq!(part1(input.as_bytes()), 58786);
         assert_eq!(part2(input.as_bytes()), 14952185856);
